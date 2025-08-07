e50c1365ed3db9e3086a66e7114d536adfa02347
Mounika-Thirupathi <thirupathim848@gmail.com>
2025-08-07 20:18:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function switchon(){
+    document.getElementById("bulb").src="https://tse4.mm.bing.net/th/id/OIP.n2oWp_XHVwaJWnrGotIChgHaE8?pid=Api&P=0&h=180"
+}
+function switchoff(){
+    document.getElementById("bulb").src="https://tse4.mm.bing.net/th/id/OIP.A47nA-dsu4mbZK275AlSOAHaG9?pid=Api&P=0&h=180"
+}
+
