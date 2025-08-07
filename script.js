d8958c7e515dd477b97352654db53bd726b71f82
Mounika-Thirupathi <thirupathim848@gmail.com>
2025-08-07 20:19:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function switchon(){
-    document.getElementById("bulb").src="https://tse4.mm.bing.net/th/id/OIP.n2oWp_XHVwaJWnrGotIChgHaE8?pid=Api&P=0&h=180"
-}
-function switchoff(){
-    document.getElementById("bulb").src="https://tse4.mm.bing.net/th/id/OIP.A47nA-dsu4mbZK275AlSOAHaG9?pid=Api&P=0&h=180"
-}
-
